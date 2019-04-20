99e51b0bc01489db7fafdc084cbb3b52368ad9a6
Marcus Downing <marcus@bang-on.net>
2019-04-20 11:44:10 +0100

    Covers and stuff

--- src/pages/books.html.h
+++ src/pages/books.html.h
@@ -1,4 +1,4 @@
-{{#extend "layout" title="Articles"}}
+{{#extend "layout" title="Books"}}
 
 {{#content "body"}}
 
@@ -18,6 +18,8 @@
 				<img class="card-img-top" src="/assets/cover-daylight-robbery.jpg">
 				<div class="card-body">
 					<h3 class="card-title">Daylight Robbery</h3>
+					<h5 class="subtitle">The Importance of Sunlight to Health</h5>
+					<p>1988</p>
 				</div>
 			</article>
 
@@ -25,6 +27,7 @@
 				<img class="card-img-top" src="/assets/cover-why-me.jpg">
 				<div class="card-body">
 					<h3 class="card-title">Why M.E.?</h3>
+					<p>1989</p>
 				</div>
 			</article>
 
@@ -33,6 +36,31 @@
 				<div class="card-body">
 					<h3 class="card-title">Soleil Vital</h3>
 					<h5 class="subtitle">Les bienfaits du soleil sur la santé</h5>
+					<p>2001</p>
+				</div>
+			</article>
+
+			<article class="card card-book">
+				<img class="card-img-top" src="/assets/cover-bon-usage-du-soleil.jpg">
+				<div class="card-body">
+					<h3 class="card-title">Du Bon Usage du Soleil</h3>
+					<p>2006</p>
+				</div>
+			</article>
+
+			<article class="card card-book">
+				<img class="card-img-top" src="/assets/cover-vitamin-cure-for-allergies.jpg">
+				<div class="card-body">
+					<h3 class="card-title">The Vitamin Cure for Allergies</h3>
+					<p>2010</p>
+				</div>
+			</article>
+
+			<article class="card card-book">
+				<img class="card-img-top" src="/assets/cover-vitamin-cure-for-digestive-disease.jpg">
+				<div class="card-body">
+					<h3 class="card-title">The Vitamin Cure for Digestive Disease</h3>
+					<p>2014</p>
 				</div>
 			</article>
 		</div>
